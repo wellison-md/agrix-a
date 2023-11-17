609a8109d9e52fb3c1cb4236bd18d2c281760339
Wellison M. Dias <wellisonmdias@gmail.com>
2023-11-17 18:26:40 -0300

    fix: fix linter

--- src/main/java/com/betrybe/agrix/utils/Exception.java
+++ src/main/java/com/betrybe/agrix/utils/Exception.java
@@ -3,7 +3,7 @@ package com.betrybe.agrix.utils;
 /**
  * The type Exception.
  */
-public class Exception extends RuntimeException{
+public class Exception extends RuntimeException {
 
   /**
    * Instantiates a new Exception.
