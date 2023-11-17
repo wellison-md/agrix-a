b3834fc3863e6d2a5d024aa58cf3b4927f7462bb
Wellison M. Dias <wellisonmdias@gmail.com>
2023-11-17 19:58:03 -0300

    feat: create Crop entity

---
+++ src/main/java/com/betrybe/agrix/entities/Crop.java
@@ -0,0 +1,90 @@
+package com.betrybe.agrix.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+/**
+ * The type Crop.
+ */
+@Entity
+@Table(name = "crop")
+public class Crop {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private double plantedArea;
+
+  /**
+  * Instantiates a new Crop.
+  *
+  * @param id          the id
+  * @param name        the name
+  * @param plantedArea the planted area
+  */
+  public Crop(Long id, String name, double plantedArea) {
+    this.id = id;
+    this.name = name;
+    this.plantedArea = plantedArea;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets name.
+   *
+   * @param name the name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Gets planted area.
+   *
+   * @return the planted area
+   */
+  public double getPlantedArea() {
+    return plantedArea;
+  }
+
+  /**
+   * Sets planted area.
+   *
+   * @param plantedArea the planted area
+   */
+  public void setPlantedArea(double plantedArea) {
+    this.plantedArea = plantedArea;
+  }
+}
